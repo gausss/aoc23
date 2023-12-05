aa611daa211d0f8fefc3b0a0c41812409bc20a92
gausss <gausssven@gmail.com>
2023-12-05 12:02:27 +0100

    first commit

---
+++ app/src/main/java/aoc23/App.java
@@ -0,0 +1,8 @@
+package aoc23;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("A: " + new Ex3().solveA());
+        System.out.println("B: " + new Ex3().solveB());
+    }
+}
